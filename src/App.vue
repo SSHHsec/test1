a713a9831d88e9902065b7a8e1ba99c5437c82ac
SSHHsec <86337870+SSHHsec@users.noreply.github.com>
2021-07-01 09:28:50 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+
+  </div>
+    <Todo></Todo>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {   
+    Todo
+  }
+}
+</script>
+
